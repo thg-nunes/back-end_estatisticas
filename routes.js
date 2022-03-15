1d0722a74d47c5b62d64e771c37c72b1d3cdb8fa
thiago <desenvolvedor.nunes@gmail.com>
2022-03-15 15:06:20 -0300

    muda metodo http

--- routes.js
+++ routes.js
@@ -13,16 +13,15 @@ const get_abertasMensal = require('./controllers/dbGetData/get_abertasMensal')
 const get_empresasAbertas = require('./controllers/dbGetData/get_empresasAbertas')
 const get_empresasAtivas = require('./controllers/dbGetData/get_empresasAtivas')
 
-routers.post('/getMunicipios', get_municipios.getMunicipios)
-routers.post('/getPorte', get_porte.getPorte)
-routers.post('/getSetor', get_setor.getSetor)
-routers.post('/getNatureza', get_natureza.getNatureza)
-routers.post('/getDescricaoAtividade', get_descricaoAtividade.getDescricaoAtividade)
-routers.post('/getSecaoAtividade', get_secaoAtividade.getSecaoAtividade)
+routers.get('/getMunicipios', get_municipios.getMunicipios)
+routers.get('/getPorte', get_porte.getPorte)
+routers.get('/getSetor', get_setor.getSetor)
+routers.get('/getNatureza', get_natureza.getNatureza)
+routers.get('/getDescricaoAtividade', get_descricaoAtividade.getDescricaoAtividade)
+routers.get('/getSecaoAtividade', get_secaoAtividade.getSecaoAtividade)
 routers.post('/getAbertasAnual', get_abertasAnual.getAbertasAnual)
 routers.post('/getAbertasMensal', get_abertasMensal.getAbertasMensal)
 routers.post('/getEmpresasAbertas', get_empresasAbertas.getEmpresasAbertas)
 routers.post('/getEmpresasAtivas', get_empresasAtivas.getEmpresasAtivas)
-
 
 module.exports = routers
