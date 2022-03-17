fdc5a05673e20fa230ebfa39f22ad2d1be2b432e
thiago <desenvolvedor.nunes@gmail.com>
2022-03-17 13:38:51 -0300

    adiciona funcao de request

--- controllers/dbGetData/get_empresasAbertas.js
+++ controllers/dbGetData/get_empresasAbertas.js
@@ -1,9 +1,24 @@
+const request = require('request')
 const forma_query = require('../gerar_query')
 
 module.exports = {
   async getEmpresasAbertas(req, res){
 
-    const response = await forma_query(req.body.classificacao, req.body.filtros)
-    return response
+    const query = await forma_query(req.body.classificacao, req.body.filtros)
+
+    request({
+      url: 'http://179.127.13.245:8099/query/sql',
+      method :"POST",
+      headers : {
+        "content-type": "application/json",
+      },
+      body: {
+        "sql": query
+      },
+      json: true
+    },
+    function (err, httpResponse, body) {
+      return res.json(body)
+    })
   }
 }
