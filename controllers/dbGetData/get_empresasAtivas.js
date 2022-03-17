0a6854774354dc61c1074a5b77f6370564deaf4f
thiago <desenvolvedor.nunes@gmail.com>
2022-03-17 14:33:59 -0300

    adiciona funcao para buscas dados de empresas ativas

--- controllers/dbGetData/get_empresasAtivas.js
+++ controllers/dbGetData/get_empresasAtivas.js
@@ -1,24 +1,9 @@
-const request = require('request')
-const forma_query = require('../gerar_query')
+const getDataEmpresasAtivas = require('../gerar_query/gerar_querys_empresas_ativas')
 
 module.exports = {
   async getEmpresasAtivas(req, res){
 
-    const query = await forma_query(req.body.classificacao, req.body.filtros)
-    
-    request({
-      url: 'http://179.127.13.245:8099/query/sql',
-      method :"POST",
-      headers : {
-        "content-type": "application/json",
-      },
-      body: {
-        "sql": query
-      },
-      json: true
-    },
-    function (err, httpResponse, body) {
-      return res.json(body)
-    })
+    const response = await getDataEmpresasAtivas(req.body.classificacao, req.body.filtros, res)
+    return response
   }
 }
