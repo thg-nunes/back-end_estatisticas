ecf53eacb605bb171efc03835223cc817c96617a
thiago <desenvolvedor.nunes@gmail.com>
2022-04-21 13:38:19 -0300

    querys sendo criadas dinamicamente

--- controllers/dbGetFIlters/filtersBuild.js
+++ controllers/dbGetFIlters/filtersBuild.js
@@ -1,25 +1,31 @@
-const getFilterData = require('../../helper/filterData');
-
 module.exports = {
   async buildFilters(req, res){
 
     const { state } = req.body
-    let initial_query = `select distinct `
-    let response_filters = []
-    let from_table = ` from statistical where `
+    let where_for_full_filters = 'where '
     
     for (const key in state) {
+      let where_to_empty_filters = ``
+      let initial_query = 'select distinct '
+      
       switch (key) {
         default:
-          if(key !== 'ano' && key !== 'empresasAbertas' && typeof state[key] === 'object') {
-             state[key].forEach((filtro, index) => {
-               index <= 0 ? from_table += `(${key} = '${filtro}' ${state[key].length == 1 ? ') and ' : ''}` : from_table += ` or ${key} = '${filtro} ${index == state[key].length -1 ? ') ' : ''}`
-             })
+          if(key !== 'ano' && key !== 'empresasAbertas' && typeof state[key] == 'object') {
+          // aqui forma as querys de busca para os filtros enquanto não tem nada selecionado
+            initial_query += `${key} from statistical where ${key} != 'null' order by ${key} limit 1500 `
+            state[key].forEach((filtro, index) => {
+              index <= 0 ? where_for_full_filters += `(${key} = '${filtro}' ${state[key].length == 1 ? ') and ' : ''}` : where_for_full_filters += ` or ${key} = '${filtro} ${index == state[key].length -1 ? ')' : ''}`
+            })
+            break
+          }
+          if (key !== 'ano' && key !== 'empresasAbertas') {
+            if(state[key] == '') {
+              initial_query += `${key} from statistical `
+              where_to_empty_filters += `${where_for_full_filters !== 'where ' ? `${key} != 'null' and ${where_for_full_filters} order by ${key} limit 1500` :`where ${key} != 'null' order by ${key} limit 1500 `}`
+              break
             }
-            console.log(`${initial_query} ${key} `)
-            console.log(`${from_table}`)
-          break;
+          }
+        }
       }
     }
-  }
 }
