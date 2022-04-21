883a853cce68862799de538ec46e75b6f7967b44
thiago <desenvolvedor.nunes@gmail.com>
2022-04-21 14:22:47 -0300

    correcao na montagem da query

--- controllers/dbGetFIlters/filtersBuild.js
+++ controllers/dbGetFIlters/filtersBuild.js
@@ -2,7 +2,7 @@ module.exports = {
   async buildFilters(req, res){
 
     const { state } = req.body
-    let where_for_full_filters = 'where '
+    let where_for_full_filters = ' '
     
     for (const key in state) {
       let where_to_empty_filters = ``
@@ -14,14 +14,14 @@ module.exports = {
           // aqui forma as querys de busca para os filtros enquanto não tem nada selecionado
             initial_query += `${key} from statistical where ${key} != 'null' order by ${key} limit 1500 `
             state[key].forEach((filtro, index) => {
-              index <= 0 ? where_for_full_filters += `(${key} = '${filtro}' ${state[key].length == 1 ? ') and ' : ''}` : where_for_full_filters += ` or ${key} = '${filtro} ${index == state[key].length -1 ? ')' : ''}`
+              index <= 0 ? where_for_full_filters += ` and (${key} = '${filtro}' ${state[key].length == 1 ? ') ' : ''}` : where_for_full_filters += ` or ${key} = '${filtro} ${index == state[key].length -1 ? ')' : ''}`
             })
             break
           }
           if (key !== 'ano' && key !== 'empresasAbertas') {
             if(state[key] == '') {
               initial_query += `${key} from statistical `
-              where_to_empty_filters += `${where_for_full_filters !== 'where ' ? `${key} != 'null' and ${where_for_full_filters} order by ${key} limit 1500` :`where ${key} != 'null' order by ${key} limit 1500 `}`
+              where_to_empty_filters += `${where_for_full_filters !== 'where ' ? `${key} != 'null' ${where_for_full_filters} order by ${key} limit 1500` :`where ${key} != 'null' order by ${key} limit 1500 `}`
               break
             }
           }
